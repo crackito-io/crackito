96d347ea989a9acabab12c3c2d88cf0b4dfacff7
Ember <enzo.louis@etu.iut-tlse3.fr>
2024-04-25 15:02:34 +0200

    #4 permissions migrations from json static file to database

---
+++ prisma/migrations/20240424091400_add_permissions/migration.sql
@@ -0,0 +1,7 @@
+-- InsertData - Permissions about users features
+INSERT INTO permission
+VALUES
+("view_users", "View users"),
+("create_users", "Create users"),
+("edit_users", "Edit or update users"),
+("delete_users", "Delete users");
