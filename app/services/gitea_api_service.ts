702f4c57e95a0c7c51be1f622f0ae40d9811311d
Gamingdy <christgik12@gmail.com>
2024-05-02 16:16:49 +0200

    #10 feat: add error management for when creating repository

--- app/services/gitea_api_service.ts
+++ app/services/gitea_api_service.ts
@@ -15,30 +15,27 @@ export class GiteaApiService {
   private owner_name: string = ''
   private access_token: string = `Bearer ${env.get('GITEA_TOKEN')}`
   private api_url: string = `${env.get('GITEA_URL')}/api/v1`
+  private http_service: HttpService = new HttpService(this.api_url, {
+    Authorization: this.access_token,
+  })
 
-  createRepository(repo_name: string) {
-    const url = `${this.api_url}/user/repos`
-
-    return new Promise<any>((resolve, reject) => {
-      try {
-        const result = this.postMethod(
-          url,
-          {
-            name: repo_name,
-            auto_init: true,
-            default_branch: 'main',
-            private: true,
-            template: true,
-          },
-          {
-            Authorization: this.access_token,
-          }
-        )
-        resolve(result)
-      } catch (error) {
-        reject(new Error(error))
+  async createRepository(repo_name: string) {
+    const url: string = `/user/repos`
+    try {
+      return await this.http_service.post(url, {
+        name: repo_name,
+        auto_init: true,
+        default_branch: 'main',
+        private: true,
+        template: true,
+      })
+    } catch (error) {
+      return {
+        status: error.response.status,
+        statusText: error.response.data.message,
+        data: error.response.config.data,
       }
-    })
+    }
   }
 
   async addMemberToRepository(repo_name: string, username: string) {
