9d3036094d6c5e857bf00068055f4b9faf186e07
Gamingdy <christgik12@gmail.com>
2024-05-02 15:12:48 +0200

    feat: create HttpService, which will be used for each http call

---
+++ app/services/http_service.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance } from 'axios'
+
+export class HttpService {
+  private default_header: object
+  private client: AxiosInstance
+
+  constructor(base_url: string, default_header: object = {}) {
+    this.default_header = default_header
+    this.client = axios.create({ baseURL: base_url })
+  }
+
+  post(url: string, body: object, headers: object = {}) {
+    headers = { ...this.default_header, ...headers }
+    return this.client.post(url, body, {
+      headers: headers,
+    })
+  }
+
+  get(url: string, headers: object = {}) {
+    headers = { ...this.default_header, ...headers }
+    return this.client.get(url, {
+      headers: headers,
+    })
+  }
+
+  put(url: string, body: object, headers: object = {}) {
+    headers = { ...this.default_header, ...headers }
+    return this.client.put(url, body, {
+      headers: headers,
+    })
+  }
+}
