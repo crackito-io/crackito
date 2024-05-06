3e620852ca8950e3adbfd353678cd56bcd351a98
Ember <enzo.louis@etu.iut-tlse3.fr>
2024-05-06 15:39:56 +0200

    #18 make migrations

---
+++ prisma/migrations/20240506133928_add_organization_federation_link/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Added the required column `id_organization` to the `federation` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `federation` ADD COLUMN `id_organization` INTEGER NOT NULL;
+
+-- CreateIndex
+CREATE INDEX `federation_ibfk1` ON `federation`(`id_organization`);
+
+-- AddForeignKey
+ALTER TABLE `federation` ADD CONSTRAINT `federation_ibfk1` FOREIGN KEY (`id_organization`) REFERENCES `organization`(`id_organization`) ON DELETE NO ACTION ON UPDATE NO ACTION;
