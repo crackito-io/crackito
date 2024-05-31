e4fcd266654b9057bf91f3f9bf55b18e93871b40
Ember <enzo.louis@etu.iut-tlse3.fr>
2024-05-31 16:52:32 +0200

    #51 fix duplicate team repo name bug

--- prisma/migrations/20240528205210_team_repo_name_unique/migration.sql
+++ prisma/migrations/20240528205210_team_repo_name_unique/migration.sql
@@ -1,2 +1,15 @@
+WITH DuplicateRows AS (
+  SELECT
+    id_team,
+    team_repo_name,
+    ROW_NUMBER() OVER (PARTITION BY team_repo_name ORDER BY id_team) AS row_num
+	FROM team
+)
+
+UPDATE team
+JOIN DuplicateRows ON team.id_team = DuplicateRows.id_team
+SET team.team_repo_name = CONCAT(team.team_repo_name, '_', DuplicateRows.row_num)
+WHERE DuplicateRows.row_num > 1;
+
 -- CreateIndex
 CREATE UNIQUE INDEX `team_repo_name` ON `team`(`team_repo_name`);
